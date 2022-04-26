9067f3742e6832953e7b4d292821f2016928b42b
ChuSeungDeok <cntmdejrsla@naver.com>
2022-04-26 15:26:19 +0900

    JS : NAVBAR 메뉴 클릭

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 'use strict'
-
+// navbar가 내려가면 투명하게
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', ()=>{
-    console.log(window.scrillY);
     if (window.scrollY > navbarHeight){
         navbar.classList.add('navbar--dark');
     } else {
         navbar.classList.remove('navbar--dark');
     }   
+});
+
+// navbar 메뉴 클릭시 스크롤링
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event)=>{
+    // console.log(event.target.dataset.link);
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior : "smooth"});
 });
